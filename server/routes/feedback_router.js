4823697a30282291960851e36c852962f8657535
Sean Wade <smwade1115@gmail.com>
2021-01-23 16:02:49 -0600

    posting to database working

--- server/routes/feedback_router.js
+++ server/routes/feedback_router.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.post('/', (req, res) => {
-    console.log('in post');
-
+    const fullFeedback = req.body;
+    console.log('in post, received', fullFeedback);
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);`
+    pool.query(queryText, [fullFeedback.feeling, fullFeedback.understanding, fullFeedback.support, fullFeedback.comments])
+        .then((result) => {
+            console.log('added to db');
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('received error', error);
+            res.sendStatus(500);
+        })
 })
 
 module.exports = router;
--- src/components/Review.jsx/Review.jsx
+++ src/components/Review.jsx/Review.jsx
@@ -4,13 +4,14 @@ import { useHistory } from 'react-router-dom';
 
 function Review() {
 
-    const feeling = useSelector(store => store.feelingReducer);
-    const understanding = useSelector(store => store.understandingReducer);
-    const support = useSelector(store => store.supportReducer);
+    const feeling = Number(useSelector(store => store.feelingReducer));
+    const understanding = Number(useSelector(store => store.understandingReducer));
+    const support = Number(useSelector(store => store.supportReducer));
     const comments = useSelector(store => store.commentReducer);
     const history = useHistory();
 
     const handleNext = () => {
+        console.log('in handle next');
         const fullFeedback = { feeling, understanding, support, comments };
         console.log(fullFeedback);
         axios.post('/feedback', fullFeedback)
@@ -22,14 +23,14 @@ function Review() {
     }
 
     return (
-        <form>
+        <div>
             <h3>Review Your Feedback</h3>
             <h5>Feeling : {feeling}</h5>
             <h5>Understanding : {understanding}</h5>
             <h5>Support : {support}</h5>
             <h5>Comment : {comments}</h5>
             <button onClick={handleNext}>Submit</button>
-        </form>
+        </div>
     )
 }
 
